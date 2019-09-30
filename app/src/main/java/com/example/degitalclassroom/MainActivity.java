b7af1f4b86c274f000e0f5b2686ec586cea8ba5b
Pooja <poojapatle19071996@gmail.com>
2019-09-30 10:23:27 +0530

    30/09/2019

--- app/src/main/java/com/example/degitalclassroom/MainActivity.java
+++ app/src/main/java/com/example/degitalclassroom/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
