dbfceca1c41e0fbdd78f9ffe22793a4fba5ddb53
Pooja <poojapatle.2211@gmail.com>
2019-10-05 13:13:28 +0530

    05/10/2019

---
+++ app/src/main/java/com/example/degitalclassroom/activity/DetailsPdfActivity.java
@@ -0,0 +1,34 @@
+package com.example.degitalclassroom.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.degitalclassroom.R;
+
+public class DetailsPdfActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_pdf);
+
+        toolbar=(Toolbar)findViewById(R.id.tool_bar);
+        toolbar.setTitle("Details");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                onBackPressed();
+            }
+        });
+    }
+}
